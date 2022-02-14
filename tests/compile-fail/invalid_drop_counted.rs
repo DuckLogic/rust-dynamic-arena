c8d8dccea4cabbfa29f35bf8ceb0a9a0bcc9a941
Techcable <Techcable@techcable.net>
2022-02-13 17:39:37 -0700

    Switch from compiletest_rs -> trybuild

--- tests/compile-fail/invalid_drop_counted.rs
+++ tests/compile-fail/invalid_drop_counted.rs
@@ -2,7 +2,6 @@ extern crate dynamic_arena;
 
 use dynamic_arena::DynamicArena;
 use std::cell::Cell;
-use std::ops::Drop;
 
 pub struct DropCounted<'a>(&'a Cell<u32>);
 
@@ -10,9 +9,8 @@ fn main() {
     let arena = DynamicArena::new_bounded();
     {
         let cell = Box::new(Cell::new(0));
-        for i in 0..5 {
+        for _i in 0..5 {
             arena.alloc(DropCounted(&cell));
-            //~^ ERROR `cell` does not live long enough
         }
     }
     /*
--- tests/compiletest.rs
+++ tests/compiletest.rs
@@ -1,21 +1,6 @@
-extern crate compiletest_rs as compiletest;
-
-use std::path::PathBuf;
-
-fn run_mode(mode: &'static str) {
-
-    let mut config = compiletest::Config::default();
-    let cfg_mode = mode.parse().expect("Invalid mode");
-
-    config.mode = cfg_mode;
-    config.src_base = PathBuf::from(format!("tests/{}", mode));
-    config.link_deps();
-    config.clean_rmeta();
-
-    compiletest::run_tests(&config);
-}
 
 #[test]
 fn compile_test() {
-    run_mode("compile-fail");
+    let tests = trybuild::TestCases::new();
+    tests.compile_fail("tests/compile-fail/invalid_drop_counted.rs");
 }
